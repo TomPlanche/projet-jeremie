c507d6e02d7698c6475e4d8f95525e99a89ae400
Tom Planche <Tomplanche@icloud.com>
2024-02-07 11:30:55 +0100

    [19] Added the `-o` cli option. It allows to output the result to a json file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use clap::Parser;
+use edit_distance::edit_distance;
+use serde::{Deserialize, Serialize};
 ///
 /// # main.rs
 /// Little script to help Jeremie Arné for its project.
@@ -8,21 +11,11 @@
 ///
 /// ## Author
 /// Tom Planche <tomplanche.fr|github.com/tomPlanche>
-
-// Imports  ==============================================================================  Imports use std::fs::File;
-use std::{
-    fs::File,
-    io::{Read},
-    collections::HashMap,
-    path::PathBuf,
-};
-use edit_distance::edit_distance;
-use clap::{Parser};
-use serde::{Deserialize, Serialize};
+// Imports  ==============================================================================  Imports
+use std::{collections::HashMap, fs::File, io::Read, path::PathBuf};
 
 // Variables  =========================================================================== Variables
 const ROOT_DIR: &str = "/Users/tom_planche/Desktop/Prog/Rust/projet-jeremie";
-
 
 // Cli parser
 #[derive(Parser)]
@@ -50,6 +43,11 @@ struct Cli {
     /// Optional 'print-occurences' argument.
     #[arg(short, long)]
     print_occurences: bool,
+
+    /// Output the vector of occurences in a json file
+    /// Optional 'output-occurences' argument.
+    #[arg(short, long)]
+    output_occurences: bool,
 }
 
 // Types
@@ -60,10 +58,7 @@ struct SearchString {
 }
 
 type Match = Vec<String>;
-type Occurences<'a> = HashMap<
-    &'a str,
-    Match
->;
+type Occurences<'a> = HashMap<&'a str, Match>;
 // Functions  =========================================================================== Functions
 ///
 /// # find_approx_match
@@ -88,11 +83,7 @@ type Occurences<'a> = HashMap<
 ///
 /// ## Returns
 /// The number of occurences.
-fn find_approx_match(
-    line: &str,
-    string: &str,
-    max_distance: usize,
-) -> (usize, Vec<String>) {
+fn find_approx_match(line: &str, string: &str, max_distance: usize) -> (usize, Vec<String>) {
     let words_iter = line.split_whitespace();
     let window_size = string.split_whitespace().count();
 
@@ -128,21 +119,41 @@ fn find_approx_match(
 ///
 /// ## Returns
 /// A Vec of SearchString.
-fn load_from_json(
-    file_path: &PathBuf,
-) -> Vec<SearchString> {
+fn load_from_json(file_path: &PathBuf) -> Vec<SearchString> {
     // Open the file
     let mut file = File::open(file_path).expect("The file could not be opened");
 
     // Read the contents of the file into a string
     let mut json_string = String::new();
-    file.read_to_string(&mut json_string).expect("The file could not be read");
+    file.read_to_string(&mut json_string)
+        .expect("The file could not be read");
 
     // the json file is an array of objects
-    let json: Vec<SearchString> = serde_json::from_str(&json_string).expect("The json file is not valid");
+    let json: Vec<SearchString> =
+        serde_json::from_str(&json_string).expect("The json file is not valid");
 
     json
 }
+
+///
+/// # export_to_json
+/// Export the final result to a json file.
+///
+/// ## Arguments
+/// * `occurences` - The occurences to export.
+/// * `file_path` - The path to the json file.
+///
+/// ## Returns
+/// Nothing.
+fn export_to_json(occurences: Occurences, file_path: &PathBuf) {
+    // Open the file
+    let file = File::create(file_path).expect("The file could not be created");
+
+    // Write the occurences to the file
+    serde_json::to_writer_pretty(&file, &occurences)
+        .expect("The occurences could not be written to the file");
+}
+
 // Main  ====================================================================================  Main
 fn main() {
     // Change the current directory
@@ -154,11 +165,13 @@ fn main() {
     let debug = cli.debug;
     let print_occurences = cli.print_occurences;
     let strings_file = cli.strings_file;
+    let output_occurences = cli.output_occurences;
 
     // Read the transcript
     let mut file = File::open(file_2_read).expect("Error while opening the file");
     let mut content = String::new();
-    file.read_to_string(&mut content).expect("Error while reading the file");
+    file.read_to_string(&mut content)
+        .expect("Error while reading the file");
 
     // Load the strings to search
     let strings = load_from_json(&PathBuf::from(strings_file));
@@ -187,13 +200,17 @@ fn main() {
         }
     }
 
-    println!("Occurences:");
     if print_occurences {
+        println!("Occurences:");
         println!("{:#?}", occurences);
     } else {
-        for (string, matches) in occurences {
+        for (string, matches) in &occurences {
             println!("{}: {}", string, matches.len());
         }
+    }
+
+    if output_occurences {
+        export_to_json(occurences, &PathBuf::from("./src/outputs/occurences.json"));
     }
 }
 
